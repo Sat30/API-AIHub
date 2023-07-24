60aeb8904245de9fb8a605a647a24e522ac577bc
Sat30 <satishhuddar03@gmail.com>
2023-07-24 22:57:00 +0530

    Initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
     <div className='fixed top-0 left-0 right-0 z-50 flex items-center justify-between h-20 border-b shadow-sm backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 border-slate-300 dark:border-slate-700'>
       <div className='container flex items-center justify-between w-full mx-auto max-w-7xl'>
         <Link href='/' className={buttonVariants({ variant: 'link' })}>
-          Text Similarity v1.0
+          AI-APIHub
         </Link>
 
         <div className='md:hidden'>
